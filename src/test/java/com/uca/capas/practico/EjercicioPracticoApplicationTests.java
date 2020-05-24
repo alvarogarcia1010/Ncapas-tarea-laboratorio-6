971c5b20f2fcc14cb1083f29fc24b8a2807a9a50
alvarogarcia1010 <00111317@uca.edu.sv>
2020-05-24 16:10:17 -0600

    Initial project

---
+++ src/test/java/com/uca/capas/practico/EjercicioPracticoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uca.capas.practico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioPracticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
